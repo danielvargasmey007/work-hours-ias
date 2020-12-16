9094ee63fcb4a67fb09c5c9531b8a8a670b08984
Anderson Daniel Vargas Martinez <danielvargasmey007@gmail.com>
2020-12-15 20:55:09 -0500

    spring project is initialized

---
+++ workhours-back/src/main/java/com/ias/workhours/WorkhoursApplication.java
@@ -0,0 +1,13 @@
+package com.ias.workhours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkhoursApplication.class, args);
+	}
+
+}
---
+++ workhours-back/src/test/java/com/ias/workhours/WorkhoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ias.workhours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkhoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
